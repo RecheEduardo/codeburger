bc67ec46eddeef006f38d2d487fdfc5bd03421a4
RecheEduardo <Eduardorechemartins2006@gmail.com>
2025-07-03 19:23:15 -0300

    feat: adicionando botão de rodapé para acesso múltiplo de janelas

--- src/br/com/hamburgueria/view/TelaCrudPedidos.java
+++ src/br/com/hamburgueria/view/TelaCrudPedidos.java
@@ -11,29 +11,41 @@ public class TelaCrudPedidos extends JFrame{
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(900, 650);
         this.setLocationRelativeTo(null);
+
+        this.setLayout(new BorderLayout());
         
         final Font fonteAppBold = new Font("Century Gothic", Font.BOLD, 36);
-        // final Font fonteAppRegular = new Font("Century Gothic", Font.PLAIN, 24);
+        final Font fonteAppRegular = new Font("Century Gothic", Font.PLAIN, 24);
 
-        JPanel painelLogin = new JPanel();
-        painelLogin.setLayout(new BoxLayout(painelLogin, BoxLayout.Y_AXIS));
-        painelLogin.setBorder(BorderFactory.createEmptyBorder(20, 10, 20, 10));
-        this.add(painelLogin);
+        JPanel painelConteudo = new JPanel();
+        painelConteudo.setLayout(new BoxLayout(painelConteudo, BoxLayout.Y_AXIS));
+        painelConteudo.setBorder(BorderFactory.createEmptyBorder(20, 10, 20, 10));
+        this.add(painelConteudo, BorderLayout.CENTER); // Add o painel de conteúdo principal ao centro
         
         // SEPARADOR DE SEÇÃO
         JSeparator separador = new JSeparator(SwingConstants.HORIZONTAL);
         separador.setForeground(Color.BLACK);
         separador.setMaximumSize(new Dimension(Integer.MAX_VALUE, 1)); // faz a linha preencher horizontalmente
 
-        // Header Principal
+        // HEADER PRINCIPAL
         JLabel labelTitulo = new JLabel("<html>Operações de Pedidos</html>");
         labelTitulo.setFont(fonteAppBold);
         labelTitulo.setAlignmentX(Component.LEFT_ALIGNMENT);
         
-        painelLogin.add(labelTitulo);
-        painelLogin.add(Box.createVerticalStrut(15));
+        painelConteudo.add(labelTitulo);
+        painelConteudo.add(Box.createVerticalStrut(15));
         
-        painelLogin.add(separador);
-        painelLogin.add(Box.createVerticalStrut(10));
+        painelConteudo.add(separador);
+        painelConteudo.add(Box.createVerticalStrut(10));
+
+        // RODAPE DA JANELA
+        JPanel painelBotao = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        painelBotao.setBorder(BorderFactory.createEmptyBorder(0, 10, 10, 10));
+
+        // BOTAO DE VOLTAR
+        JButton botaoVoltar = new JButton("Voltar");
+        botaoVoltar.setFont(fonteAppRegular);
+        painelBotao.add(botaoVoltar);
+        this.add(painelBotao, BorderLayout.SOUTH);
 	}
 }
--- src/br/com/hamburgueria/view/TelaCrudProdutos.java
+++ src/br/com/hamburgueria/view/TelaCrudProdutos.java
@@ -12,28 +12,40 @@ public class TelaCrudProdutos extends JFrame {
         this.setSize(900, 650);
         this.setLocationRelativeTo(null);
         
-        final Font fonteAppBold = new Font("Century Gothic", Font.BOLD, 36);
-        // final Font fonteAppRegular = new Font("Century Gothic", Font.PLAIN, 24);
+        this.setLayout(new BorderLayout());
 
-        JPanel painelLogin = new JPanel();
-        painelLogin.setLayout(new BoxLayout(painelLogin, BoxLayout.Y_AXIS));
-        painelLogin.setBorder(BorderFactory.createEmptyBorder(20, 10, 20, 10));
-        this.add(painelLogin);
-        
+        final Font fonteAppBold = new Font("Century Gothic", Font.BOLD, 36);
+        final Font fonteAppRegular = new Font("Century Gothic", Font.PLAIN, 24);
+
+        JPanel painelConteudo = new JPanel();
+        painelConteudo.setLayout(new BoxLayout(painelConteudo, BoxLayout.Y_AXIS));
+        painelConteudo.setBorder(BorderFactory.createEmptyBorder(20, 10, 20, 10));
+        this.add(painelConteudo, BorderLayout.CENTER); // Add o painel de conteúdo principal ao centro
+
         // SEPARADOR DE SEÇÃO
         JSeparator separador = new JSeparator(SwingConstants.HORIZONTAL);
         separador.setForeground(Color.BLACK);
         separador.setMaximumSize(new Dimension(Integer.MAX_VALUE, 1)); // faz a linha preencher horizontalmente
 
-        // Header Principal
+        // HEADER PRINCIPAL
         JLabel labelTitulo = new JLabel("<html>Operações de Produtos</html>");
         labelTitulo.setFont(fonteAppBold);
         labelTitulo.setAlignmentX(Component.LEFT_ALIGNMENT);
-        
-        painelLogin.add(labelTitulo);
-        painelLogin.add(Box.createVerticalStrut(15));
-        
-        painelLogin.add(separador);
-        painelLogin.add(Box.createVerticalStrut(10));
+
+        painelConteudo.add(labelTitulo);
+        painelConteudo.add(Box.createVerticalStrut(15));
+
+        painelConteudo.add(separador);
+        painelConteudo.add(Box.createVerticalStrut(10));
+
+        // RODAPE DA JANELA
+        JPanel painelBotao = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        painelBotao.setBorder(BorderFactory.createEmptyBorder(0, 10, 10, 10));
+
+        // BOTAO DE VOLTAR
+        JButton botaoVoltar = new JButton("Voltar");
+        botaoVoltar.setFont(fonteAppRegular);
+        painelBotao.add(botaoVoltar);
+        this.add(painelBotao, BorderLayout.SOUTH);
 	}
 }
