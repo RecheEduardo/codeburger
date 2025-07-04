02866bec4776ed7a4964e091c6067dc8055ed3f5
RecheEduardo <Eduardorechemartins2006@gmail.com>
2025-07-03 21:41:55 -0300

    chore: adicionando setup do JDBC para o projeto com MySQL

--- src/br/com/hamburgueria/dao/ConnectionFactory.java
+++ src/br/com/hamburgueria/dao/ConnectionFactory.java
@@ -1,5 +1,36 @@
 package br.com.hamburgueria.dao;
 
+import java.sql.*;
+
 public class ConnectionFactory {
 
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/hamburgueria_db?useTimezone=true&serverTimezone=UTC";
+    private static final String USUARIO = "root";
+    private static final String SENHA = "";
+    
+    public static Connection getConnection() {
+        try {
+            Class.forName(DRIVER);
+            return DriverManager.getConnection(URL, USUARIO, SENHA);
+        } catch (ClassNotFoundException | SQLException e) {
+            // Se algo der errado (driver não encontrado, banco offline, usuário/senha incorretos)
+            System.err.println("Erro na conexão com o banco de dados: " + e.getMessage());
+            e.printStackTrace(); // Imprime o rastreamento do erro para ajudar na depuração.
+
+            // Lança uma RuntimeException para sinalizar que um erro grave ocorreu
+            throw new RuntimeException("Falha ao conectar ao banco de dados.", e);
+        }
+    }
+
+    public static void closeConnection(Connection conn) {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                System.err.println("Erro ao fechar a conexão: " + e.getMessage());
+            }
+        }
+    }
+    
 }
--- src/module-info.java
+++ src/module-info.java
@@ -1,3 +1,4 @@
 module codeBurguer {
 	requires java.desktop;
+	requires java.sql;
 }
