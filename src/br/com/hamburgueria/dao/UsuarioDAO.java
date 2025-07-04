00c9233d1a52edbc47c7b289c99e218114534fb4
RecheEduardo <Eduardorechemartins2006@gmail.com>
2025-07-03 21:43:13 -0300

    feat: prototipando classe de usuario & validação com o banco

--- src/br/com/hamburgueria/dao/UsuarioDAO.java
+++ src/br/com/hamburgueria/dao/UsuarioDAO.java
@@ -1,5 +1,39 @@
 package br.com.hamburgueria.dao;
 
-public class UsuarioDAO {
+import java.sql.*;
+import javax.swing.JOptionPane;
 
+public class UsuarioDAO {
+	
+	public boolean verificarLogin(String login, String senha) {
+		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		boolean check = false;
+	    
+		try {
+	        conn = ConnectionFactory.getConnection();
+	        String sql = "SELECT * FROM usuarios where login = ? AND senha = ?";
+	        ps = conn.prepareStatement(sql);
+	        ps.setString(1, login);
+	        ps.setString(2, senha);
+	        rs = ps.executeQuery();
+	        
+	        if(rs.next()) {
+	        	check = true;
+	        }
+	    } catch (SQLException ex) {
+	    	JOptionPane.showMessageDialog(null, "Erro no banco de dados: " + ex.getMessage());
+	    } finally {
+	    	try {
+	    		// fechando todos os recursos utilizados na chamada do banco
+	            if (rs != null) rs.close();
+	            if (ps != null) ps.close();
+	            if (conn != null) conn.close();
+	        } catch (SQLException ex) {
+	            JOptionPane.showMessageDialog(null, "Erro ao fechar a conexão: " + ex.getMessage());
+	        }
+	    }
+		return check;
+	}
 }
--- src/br/com/hamburgueria/model/Usuario.java
+++ src/br/com/hamburgueria/model/Usuario.java
@@ -1,5 +1,40 @@
 package br.com.hamburgueria.model;
 
 public class Usuario {
+	private int id;
+    private String nome;
+    private String login;
+    private String senha;
+    
+    // id
+    public Integer getID() {
+        return id;
+    }
+    public void setID(int ID) {
+    	this.id = ID;
+    }
+    
+    // username
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String Nome) {
+    	this.nome = Nome;
+    }
 
+    // email
+    public String getLogin() {
+        return login;
+    }
+    public void setLogin(String Login) {
+    	this.login = Login;
+    }
+
+    // senha
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String Senha) {
+    	this.senha = Senha;
+    }
 }
