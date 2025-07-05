5534ccb4f0ab9f16448d456ba2adff589a11eed7
RecheEduardo <Eduardorechemartins2006@gmail.com>
2025-07-04 21:26:39 -0300

    feat: criando DAO de acesso aos produtos

--- src/br/com/hamburgueria/dao/ProdutoDAO.java
+++ src/br/com/hamburgueria/dao/ProdutoDAO.java
@@ -1,5 +1,50 @@
 package br.com.hamburgueria.dao;
 
-public class ProdutoDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.swing.JOptionPane;
+
+import br.com.hamburgueria.model.Produto;
+
+public class ProdutoDAO {
+    public List<Produto> listarProdutos() {
+		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		List<Produto> produtos = new ArrayList<>();
+	    
+		try {
+	        conn = ConnectionFactory.getConnection();
+	        String sql = "SELECT * FROM produtos";
+	        ps = conn.prepareStatement(sql);
+	        rs = ps.executeQuery();
+	        
+	        while(rs.next()) {
+	        	Produto produto = new Produto();
+				produto.setId(rs.getInt("idProduto"));
+				produto.setNome(rs.getString("nomeProduto"));
+				produto.setTipo(rs.getString("tipoProduto"));
+				produto.setPreco(rs.getDouble("preco"));
+				produto.setDescricao(rs.getString("descricao"));
+                produtos.add(produto);
+	        }
+	    } catch (SQLException ex) {
+	    	JOptionPane.showMessageDialog(null, "Erro no banco de dados: " + ex.getMessage());
+	    } finally {
+	    	try {
+	    		// fechando todos os recursos utilizados na chamada do banco
+	            if (rs != null) rs.close();
+	            if (ps != null) ps.close();
+	            if (conn != null) conn.close();
+	        } catch (SQLException ex) {
+	            JOptionPane.showMessageDialog(null, "Erro ao fechar a conexão: " + ex.getMessage());
+	        }
+	    }
+		return produtos;
+	}
 }
--- src/br/com/hamburgueria/model/Produto.java
+++ src/br/com/hamburgueria/model/Produto.java
@@ -1,5 +1,49 @@
 package br.com.hamburgueria.model;
 
 public class Produto {
+	private int id;
+	private String nome;
+	private String tipo;
+	private double preco;
+	private String descricao;
 
+	// id
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	// nome
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	// tipo
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	// preco
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	// descricao
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 }
